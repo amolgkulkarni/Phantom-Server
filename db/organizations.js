18ee685528d3ff74c53cd80dd1c17bae95ed1256
Prakashchandsuthar <codezero.hack@gmail.com>
2015-02-11 12:03:41 +0530

    DB files added

---
+++ db/organizations.js
@@ -0,0 +1,8 @@
+[
+            {_id: 1, name: 'Synerzip', chart: 'area-chart.png', projects: ['Google','PDX', 'Halliburton'], owner: ['Vinayak', 'Hemant']},
+            {_id: 2, name: 'Starks', chart: 'area-chart.png', projects: ['Loven', 'Halliburton'], owner: ['Mukund']},
+            {_id: 3, name: 'Lanisters', chart: 'area-chart.png', projects: ['LRS','Zimbra', 'Mercatus'], owner: ['Ashutosh']},
+            {_id: 4, name: 'Targareyen', chart: 'area-chart.png', projects: ['CMS','Studer'], owner: ['Preshit']},
+            {_id: 5, name: 'Baratheon', chart: 'area-chart.png', projects: ['PDX'], owner: ['Anil']},
+            {_id: 6, name: 'Tryrell', chart: 'area-chart.png', projects: ['CloudOn','Prezi'], owner: ['Amit Bakore']}
+        ]
